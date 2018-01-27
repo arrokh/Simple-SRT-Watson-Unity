d40f67c81c60d1c3f88bf91a43fe1d816d96cad3
Noor Octavian Anwar <alv1n@live.com>
2018-01-28 05:54:18 +0700

    Refactory project

---
+++ Assets/Simple-SRT-Watson-Unity/com/ARHandler/StopARCamera.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Vuforia;
+
+namespace SRTWatsonUnity.com.ARHandler
+{
+    public class StopARCamera : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+            Camera mainCamera = Camera.main;
+            if (mainCamera)
+            {
+                if (mainCamera.GetComponent<VuforiaBehaviour>() != null)
+                    mainCamera.GetComponent<VuforiaBehaviour>().enabled = false;
+
+                if (mainCamera.GetComponent<VideoBackgroundBehaviour>() != null)
+                    mainCamera.GetComponent<VideoBackgroundBehaviour>().enabled = false;
+
+                //if (mainCamera.GetComponent<DefaultInitializationErrorHandler>() != null)
+                //    mainCamera.GetComponent<DefaultInitializationErrorHandler>().enabled = false;
+
+                //mainCamera.clearFlags = CameraClearFlags.Skybox;
+            }
+        }
+
+    }
+}
---
+++ Assets/Simple-SRT-Watson-Unity/com/SceneHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SRTWatsonUnity.com
+{
+    public class SceneHandler : MonoBehaviour
+    {
+
+        public void Load(string scene)
+        {
+            SceneManager.LoadScene(scene);
+        }
+
+        public void Load(int scene)
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
