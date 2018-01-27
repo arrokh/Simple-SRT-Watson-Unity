fe5f001ef4c5ce6e6eb55d759352ed515569033d
Noor Octavian Anwar <alv1n@live.com>
2018-01-27 22:13:25 +0700

    Add init library

---
+++ Assets/Scripts/AddCommandToPanel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddCommandToPanel : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_targetPanel;
+
+    [SerializeField]
+    private GameObject m_prefabButton;
+
+    private PlayerControllerHandler m_playerControllerHandler;
+
+    void Start()
+    {
+        m_playerControllerHandler = GetComponent<PlayerControllerHandler>();
+
+        foreach (var item in m_playerControllerHandler.GetAllWords())
+            AddPrefabButton(item);
+    }
+
+    private void AddPrefabButton(string text)
+    {
+        GameObject target = Instantiate(m_prefabButton);
+        target.transform.SetParent(m_targetPanel);
+        target.transform.position = Vector3.zero;
+        target.transform.localScale = Vector3.one;
+        target.GetComponentInChildren<Text>().text = text;
+    }
+}
---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+
+    public void Load(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Load(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
