c149f1c36243819cdd8ccc7d1c806f6039ae3466
Noor Octavian Anwar <alv1n@live.com>
2018-01-28 05:50:37 +0700

    Create AnimatorHandlerSRT to show how handling animator with SSRTWatsonUnity

---
+++ Assets/Simple-SRT-Watson-Unity/com/AnimatorHandlerSRT.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SRTWatsonUnity.com
+{
+    public class AnimatorHandlerSRT : MonoBehaviour
+    {
+        [SerializeField]
+        private string[] m_statesName;
+
+        private Animator m_animator;
+        private SimpleSRTWatsonUnity m_srtWatson;
+
+        private void Start()
+        {
+            m_animator = GetComponent<Animator>();
+            m_srtWatson = FindObjectOfType<SimpleSRTWatsonUnity>();
+        }
+
+        private void ChangeState(string state)
+        {
+            m_animator.SetBool(state, true);
+            Invoke("UnCheckAllState", 0.3f);
+        }
+
+        private void UnCheckAllState()
+        {
+            foreach (var stateName in m_statesName)
+                m_animator.SetBool(stateName, false);
+        }
+
+
+    }
+
+}
